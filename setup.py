46b9aa24833961e583a70799d7a7a49767e61b66
sparklost <marko.b.zivic@gmail.com>
2026-07-11 11:39:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,24 @@ import shutil
 import subprocess
 import sys
 
-LIBRARIES = (
-    "apsw",
-    "psycopg",
-)
+PACKAGES = ["apsw==3.53.3.1", "psycopg==3.3.4"]
+TERMUX_PACKAGES = ["apsw"]
 
 
-def termux_install_apsw():
-    """Setup apsw on termux"""
-    lib = "apsw"
-    if not shutil.which("apsw"):
-        subprocess.run(["pkg", "i", f"python-{lib}"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    current_dir = os.getcwd()
-    system_dir = glob.glob(os.path.expandvars("$PREFIX/lib/python*/site-packages"))[0]
-    shutil.copytree(os.path.join(system_dir, lib), os.path.join(current_dir, lib))
+def install_packages_termux(packages):
+    """Install specified packages in extension dir, for termux"""
+    for package in packages:
+        if not shutil.which(package):
+            subprocess.run(["pkg", "i", f"python-{package}"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        current_dir = os.getcwd()
+        system_dir = glob.glob(os.path.expandvars("$PREFIX/lib/python*/site-packages"))[0]
+        shutil.copytree(os.path.join(system_dir, package), os.path.join(current_dir, package))
 
 
-def main():
-    """Setup environment"""
-    if sys.platform =="android" and shutil.which("termux-backup"):
-        termux_install_apsw()
-        return
-    subprocess.run(["virtualenv", "env"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    for lib in LIBRARIES:
+def install_packages(libraries):
+    """Install specified packages in extension dir"""
+    subprocess.run(["python", "-m", "venv", "env"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    for lib in libraries:
         subprocess.run(
             ["./env/bin/python", "-m", "pip", "install", "--target=temp", lib],
             check=True,
@@ -37,10 +32,20 @@ def main():
     temp_dir = os.path.join(current_dir, "temp")
     if not os.path.exists(temp_dir):
         return
-    for lib in LIBRARIES:
-         shutil.move(os.path.join(temp_dir, lib), os.path.join(current_dir, lib))
+    for lib in libraries:
+        lib_name = lib.split("<")[0].split(">")[0].split("=")[0]
+        shutil.move(os.path.join(temp_dir, lib_name), os.path.join(current_dir, lib_name))
     shutil.rmtree(temp_dir)
     shutil.rmtree(os.path.join(current_dir, "env"))
+
+
+def main():
+    """Setup environment"""
+    if sys.platform =="android" and shutil.which("termux-backup"):
+        install_packages_termux(TERMUX_PACKAGES)
+        install_packages([x for x in PACKAGES if any(x.startswith(y) for y in TERMUX_PACKAGES)])
+        return
+    install_packages(PACKAGES)
 
 
 if __name__ == "__main__":
